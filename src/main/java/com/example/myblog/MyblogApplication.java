b67171fcfcb89a5de6621393735c9e6433340964
imjaeyeol <imjy2@naver.com>
2023-05-17 15:13:01 +0900

    2023051 first commit

--- src/main/java/com/example/myblog/MyblogApplication.java
+++ src/main/java/com/example/myblog/MyblogApplication.java
@@ -1,11 +1,26 @@
 package com.example.myblog;
 
+import com.example.myblog.config.auth.PrincipalDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableJpaAuditing
+@RequiredArgsConstructor
 @SpringBootApplication
 public class MyblogApplication {
-
+    private final PrincipalDetailService principalDetailService;
+    @Bean
+    public BCryptPasswordEncoder encoderPW(){
+        return new BCryptPasswordEncoder();
+    }
+    protected void config(AuthenticationManagerBuilder builder)throws Exception{
+        builder.userDetailsService(principalDetailService).passwordEncoder(encoderPW());
+    }
     public static void main(String[] args) {
         SpringApplication.run(MyblogApplication.class, args);
     }
---
+++ src/main/java/com/example/myblog/service/BoardService.java
@@ -0,0 +1,10 @@
+package com.example.myblog.service;
+
+
+
+
+public class BoardService {
+
+
+
+}
